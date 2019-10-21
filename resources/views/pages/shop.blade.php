686154bd3c19e6c0cfdaf4e823fd851840aa0087
abartolomego18dw <abartolomego18dw@ikzubirimanteo.com>
2019-10-21 23:47:11 +0200

    Añadiendo modificaciones en la view shops

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.master')
 @section('content')
-<?php echo $infoShop->name?>
+<h1><?php echo $infoShop->name?></h1>
+<br>
+<?php echo $infoShop->description ?>
+<br>
+<?php echo $infoShop->logo ?>
+<br>
+<?php echo $infoShop->photo ?>
+<br>
+<?php echo $infoShop->openh ?>-<?php echo $infoShop->closeh ?>
+
 @stop
