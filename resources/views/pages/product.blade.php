ec7a5796f2f4e29c17d799af9fb92416d4c40787
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-10-21 13:43:13 +0200

    He solucionado redirección tras eliminar un producto

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -4,33 +4,24 @@
 <section id="productContainer">
 
     <h1>{{$info->name}}</h1>
-   
+
     <img src={{asset("assets/images/fotoProducto/$info->photo")}}>
-    <p><?php echo $info->description ?></p>
-
+    <p> {{$info->description}} </p>
 </section>
-
-
-<h1>Formulario update</h1>
-<form action="<?php echo '/producto/' . $info->id ?>" method='POST'>
-    @method('PUT')
-    @csrf
-    <label>stock</label>
-    <input type="number" value="<?php echo $info->stock ?>" min="1" max="20" step="1" name="stock"><br>
-    <?php //<input type="submit"> 
-    ?>
-    <input type="image" src="{{asset('assets/images/icons/update.png')}}">
-</form>
-<h1>Formulario delete</h1>
-<form action="<?php echo '/producto/' . $info->id ?>" method="POST">
-    @method('DELETE')
-    @csrf
-    <label>Eliminar</label>
-    <input type="number" min="1" max="20" step="1" name="stock"><br>
-    <?php //<input type="submit" name="Eliminar producto"> 
-    ?>
-    <input type="image" src="{{asset('assets/images/icons/delete.png')}}">
-</form>
-
+<section class="forms">
+    <form action="<?php echo '/producto/' . $info->id ?>" method='POST'>
+        @method('PUT')
+        @csrf
+        <label>Stock</label>
+        <input type="number" value={{ $info->stock }} min="1" max="20" step="1" name="stock"><br>
+        <input type="image" src="{{asset('assets/images/icons/update.png')}}">
+    </form>
+    <form action="<?php echo '/producto/' . $info->id ?>" method="POST">
+        @method('DELETE')
+        @csrf
+        <input type="hidden"  name ="shopId" value={{$info->shopId}}>
+        <input type="image" src="{{asset('assets/images/icons/delete.png')}}">
+    </form>
+</section>
 <?php echo $info ?>
 @stop
