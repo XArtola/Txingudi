662b66d3dd8c9de4e2e9e0d187265ed7b528e5ea
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-10-23 08:51:22 +0200

    Resolución de unos conflictos

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -19,22 +19,10 @@
     <div class="productos">
        
     </div>
-<<<<<<< HEAD
-=======
 
 
-<h1><?php echo $infoShop->name?></h1>
-<br>
-<?php echo $infoShop->description ?>
-<br>
-<?php echo $infoShop->logo ?>
-<br>
-<?php echo $infoShop->photo ?>
-<br>
-<?php echo $infoShop->openh ?>-<?php echo $infoShop->closeh ?>
-<img src={{asset("assets/$infoShop->photo")}}>
 
->>>>>>> e27b4c97f72252f100e4ce6aecf5b17ee97b794e
+
     <style>
     .section{
         width:100%;
