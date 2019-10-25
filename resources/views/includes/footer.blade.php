7daa5ab83792e689604e0c0362c7c06f57ef4a1f
AsiFernandez <afernandezce17dw@ikzubirimanteo.com>
2019-10-25 13:55:03 +0200

    landing, footer y estilos/traducciones

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,8 @@
+
 <!-- Código interno del footer no hace falta etiquieta footer aqui-->
+<div id="footer">
+
+    <div class="trn" data-trn-key="schedule" id="info1">HORARIO: <br>de 10:00am a 22:00pm</div>
+    <div id="info2"><img src="{{asset('assets/images/imagenesLanding/facebook.jpg')}}" style="width:50px; height:50px;"> <img src="{{asset('assets/images/imagenesLanding/twitter.jpg')}}" style="width:50px; height:50px;"> <img src="{{asset('assets/images/imagenesLanding/instagram.jpg')}}" style="width:50px; height:50px;"> <h3>Txingudi &copy RETO 0.5</h3></div>
+
+</div>
