d889f7e0308ead60e03f8b7640d8375a09dafbc8
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-10-26 12:10:21 +0200

    Solucionado el problema con script de control de productos por idioma

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,6 +6,7 @@ https://scotch.io/tutorials/simple-laravel-layouts-using-blade
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <title>Txingudi</title>
+
 <head>
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/CSS/estilos.css') }}" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -16,7 +17,6 @@ https://scotch.io/tutorials/simple-laravel-layouts-using-blade
         @import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
         @import url('https://fonts.googleapis.com/css?family=Comfortaa&display=swap');
         @import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
-
     </style>
     <script src="{{asset('assets/lib/jquery-3.4.1.js')}}" type="text/javascript"></script>
     <script src="{{asset('assets/lib/jquery.translate.js')}}" type="text/javascript"></script>
@@ -50,8 +50,32 @@ https://scotch.io/tutorials/simple-laravel-layouts-using-blade
                 /*Refrescar página si hay un form en ella*/
                 if (hasForm)
                     location.reload();
+                /*Controlar si estamos en la tienda*/
+                if (isShop) {
+                    var language = sessionStorage.getItem("language");
+                    switch (language) {
+                        case "es":
+                        $('.producto').show();
+                        $('.producto[data-language="eu"]').hide();
+                        $('.producto[data-language="en"]').hide();
+                        break;
+                        case "en":
+                        $('.producto').show();
+                        $('.producto[data-language="eu"]').hide();
+                        $('.producto[data-language="es"]').hide();
+                        break;
+                        case "eu":
+                        $('.producto').show();
+                        $('.producto[data-language="es"]').hide();
+                        $('.producto[data-language="en"]').hide();
+                        break;
+                        default:
+                        $('.producto').show();
+                        break;
 
-
+                    }
+                    $()
+                }
             });
         });
     </script>
