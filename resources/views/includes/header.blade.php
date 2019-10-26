c0c76ad80b503520c88fc7cd6773ebb86b42480e
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-10-26 18:57:25 +0200

    Shop responsive

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,10 @@
 <!-- Código interno del header no hace falta etiquieta header aqui-->
 <div class="navbar">
-    <a href="/"><img src="{{asset('assets/images/imagenesLanding/txingudi_color.png')}}"></a>
+    <a href="{{url('/')}}"><img src="{{asset('assets/images/imagenesLanding/txingudi_color.png')}}"></a>
     <img class="menuIcon" src="{{asset('assets/images/icons/menu.png')}}">
-
     <ul class="nav">
         <!--<li class="forma3"><a><span class="trn" data-trn-key="menuHome" href="{{url('/')}}">Inicio</span></a></li>-->
-        <li class="forma3"><a><img src="{{asset('assets/images/icons/home.png')}}"></a></li>
+        <li class="forma3"><img src="{{asset('assets/images/icons/home.png')}}"></a></li>
         <li class="forma2">
             <div class="dropdown">
                 <!--<button class="dropbtn trn" data-trn-key="menuShops"><img src="{{asset('assets/images/icons/cart.png')}}"></button> -->
--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -10,15 +10,14 @@
             </div>
             <p class="trn" data-trn-key='{{"description$infoShop->id"}}'><?php echo $infoShop->description ?></p>
             <div class="reloj">
-                <img src="{{asset('assets/images/icons/reloj.png')}}" width="20px" height="20px">
-                <?php echo $infoShop->openh ?>-<?php echo $infoShop->closeh ?>
+                <img src="{{asset('assets/images/icons/reloj.png')}}">
+                <span>{{$infoShop->openh }} - {{ $infoShop->closeh }}</span>
             </div>
         </div>
         <div class="section-right">
             <img src={{asset("assets/$infoShop->photo")}}>
         </div>
     </div>
-    <div class="titulo2 trn" data-trn-key="productos">Productos:</div>
     <div class="productos">
         @foreach($productos as $producto)
         <div class="producto" data-language={{ $producto->language }}>
@@ -26,8 +25,8 @@
                 <img src={{asset("assets/images/fotoProducto/$producto->photo")}}>
                 <p class="trn productName" data-trn-key='{{"productName$producto->id"}}'>{{$producto->name}}</p>
                 <!-- <p>Stock: {{$producto->stock}}</p> -->
-                <p style="color:darkgrey; font-weight: bold;">
-                    <div style="color:darkgrey; font-weight: bold;" id="precio" class="trn" data-trn-key="precio">Precio:</div> {{$producto->price}} €
+                <p>
+                    <div id="precio" class="trn" data-trn-key="precio">Precio:</div> {{$producto->price}} €
                 </p>
             </a>
         </div>
